2e59a96863b60095f6686c6870bb257b393d77fc
Michael Ross <michaelross27@gmail.com>
2022-02-14 01:26:25 -0500

    pagination added

--- onboarding-redux/src/pages/Home.js
+++ onboarding-redux/src/pages/Home.js
@@ -1,17 +1,26 @@
 import React, { useEffect } from "react";
-import { styled } from "@mui/material/styles";
+import PropTypes from "prop-types";
+import { useTheme, styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableFooter from "@mui/material/TableFooter";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import TablePagination from "@mui/material/TablePagination";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
 import { useSelector, useDispatch } from "react-redux";
 import { deleteUser, loadUsers } from "../redux/actions";
 import { history } from "../index";
-import EditUser from "./EditUser"
+import EditUser from "./EditUser";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -32,15 +41,98 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
 const Home = () => {
   let dispatch = useDispatch();
   const { users } = useSelector((state) => state.users);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - users.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   useEffect(() => {
     dispatch(loadUsers());
   }, []);
 
-  const {user} = useSelector((state) => state.users);
+  const { user } = useSelector((state) => state.users);
 
   const handleDelete = (id) => {
     if (window.confirm("Are you sure you want to delete this user?")) {
@@ -52,7 +144,7 @@ const Home = () => {
 
   const handleEdit = () => {
     setOpen(true);
-  }
+  };
 
   return (
     <div>
@@ -77,41 +169,71 @@ const Home = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {users &&
-              users.map((user) => (
-                <StyledTableRow key={user.id}>
-                  <StyledTableCell scope="row">
-                    <input type="checkbox"></input>
-                  </StyledTableCell>
-                  <StyledTableCell align="center">{user.id}</StyledTableCell>
-                  <StyledTableCell align="center">{user.name}</StyledTableCell>
-                  <StyledTableCell align="center">{user.email}</StyledTableCell>
-                  <StyledTableCell align="center">
-                    {user.address}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    {user.phoneNumber}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    {user.jobTitle}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    <Button color="primary" variant="contained" onClick={() => handleEdit(user.id)}>
-                      EDIT
-                    </Button>
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    <Button
-                      color="secondary"
-                      variant="contained"
-                      onClick={() => handleDelete(user.id)}
-                    >
-                      DELETE
-                    </Button>
-                  </StyledTableCell>
-                </StyledTableRow>
-              ))}
+            {(rowsPerPage > 0
+              ? users.slice(
+                  page * rowsPerPage,
+                  page * rowsPerPage + rowsPerPage
+                )
+              : users
+            ).map((user) => (
+              <StyledTableRow key={user.id}>
+                <StyledTableCell scope="row">
+                  <input type="checkbox"></input>
+                </StyledTableCell>
+                <StyledTableCell align="center">{user.id}</StyledTableCell>
+                <StyledTableCell align="center">{user.name}</StyledTableCell>
+                <StyledTableCell align="center">{user.email}</StyledTableCell>
+                <StyledTableCell align="center">{user.address}</StyledTableCell>
+                <StyledTableCell align="center">
+                  {user.phoneNumber}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  {user.jobTitle}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={() => handleEdit(user.id)}
+                  >
+                    EDIT
+                  </Button>
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  <Button
+                    color="secondary"
+                    variant="contained"
+                    onClick={() => handleDelete(user.id)}
+                  >
+                    DELETE
+                  </Button>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
           </TableBody>
+          <TableFooter style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={3}
+                count={users.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
         </Table>
       </TableContainer>
     </div>
